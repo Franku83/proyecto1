0727a5a6ecda053408fa1be28a3c5dba97a39ba7
Franku <franku8383@gmail.com>
2026-04-24 12:57:30 -0400

    retocando

--- inventario/authentication.py
+++ inventario/authentication.py
@@ -1,11 +1,21 @@
 from rest_framework import authentication, exceptions
+from django.contrib.auth.models import AnonymousUser
 import os
+
+class AuthenticatedAPIUser(AnonymousUser):
+    @property
+    def is_authenticated(self):
+        return True
 
 class APIKeyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         api_key = request.headers.get('X-API-Key')
         expected_key = os.getenv('API_KEY')
 
-        if api_key and expected_key and api_key == expected_key:
-            return (None, None)
+        if not expected_key:
+            return None
+
+        if api_key == expected_key:
+            return (AuthenticatedAPIUser(), None)
+        
         return None
